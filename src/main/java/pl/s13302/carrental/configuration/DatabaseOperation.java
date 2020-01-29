2e21cd193034b09668ae8788a17822685c37b73d
Rafał Podkoński <rafal.podkonski@dev-software.pl>
2020-01-29 20:35:22 +0100

    Hibernate configuration

---
+++ src/main/java/pl/s13302/carrental/configuration/DatabaseOperation.java
@@ -0,0 +1,36 @@
+package pl.s13302.carrental.configuration;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public interface DatabaseOperation {
+
+    void operation();
+
+    default void run() {
+        StandardServiceRegistry registry = null;
+        SessionFactory sessionFactory = null;
+        try {
+            registry = new StandardServiceRegistryBuilder()
+                    .configure()
+                    .build();
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sessionFactory.openSession();
+            session.beginTransaction();
+            operation();
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            StandardServiceRegistryBuilder.destroy(registry);
+        } finally {
+            if (sessionFactory != null) {
+                sessionFactory.close();
+            }
+        }
+    }
+
+}
